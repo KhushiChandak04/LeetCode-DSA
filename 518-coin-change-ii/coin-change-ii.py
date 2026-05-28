18442717549bf548f4424327de37c7f4a873e28c
Khushi Chandak <khushichandak2005@gmail.com>
2026-05-28 17:32:57 +0530

    Time: 231 ms (82.67%) | Memory: 12.8 MB (43.16%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -2,31 +2,20 @@ class Solution(object):
 
     def change(self, amount, coins):
 
-        n = len(coins)
+        #initialising dp array from 0 to target amount
+        dp = [0] * (amount + 1)
 
-        #initialising dp table, where rows = coins and cols = target from 0 to amt
-        dp = [[0 for j in range(amount + 1)] for i in range(n)]
+        #base case, there is 1 way to make target 0 i.e choose nothing
+        dp[0] = 1
 
-        #base case, where if fully divisible select all coins of same denomination
-        for target in range(amount + 1):
+        #traverse every coin one by one
+        for coin in coins:
 
-            if target % coins[0] == 0:
-                dp[0][target] = 1
+            #start from current coin till target amount
+            for target in range(coin, amount + 1):
 
-        #fill DP table
-        for index in range(1, n):
+                #adding ways of forming remaining target after taking current coin
+                dp[target] += dp[target - coin]
 
-            for target in range(amount + 1):
-
-                notTaken = dp[index - 1][target] #to not take current coin, it return to prev state
-
-                taken = 0 #to take current coin and move to next selection
-
-                if coins[index] <= target:
-
-                    taken = dp[index][target - coins[index]]
-
-                #total ways
-                dp[index][target] = notTaken + taken
-
-        return dp[n - 1][amount]
+        #return total no of ways for final target amount
+        return dp[amount]
