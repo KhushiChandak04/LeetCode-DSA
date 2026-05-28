5161d8d62c799f898ec0fc7df213ef0871d98ec3
Khushi Chandak <khushichandak2005@gmail.com>
2026-05-28 16:48:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.40%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def climbStairs(self, n):
+        dp = [-1] * (n+1) #this is how we initialise a dp, standard format
+        dp[0] = 1
+        dp[1] = 1 #for base cases
+
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2] #fibonacchi logic
+        return(dp[n])
