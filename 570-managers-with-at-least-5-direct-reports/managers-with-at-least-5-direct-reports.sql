1566363160287931677ff3de279e4f8584d8b441
Khushi Chandak <khushichandak2005@gmail.com>
2026-06-15 22:08:44 +0530

    Time: 400 ms (15.90%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -4,4 +4,4 @@ FROM Employee e
 JOIN Employee emp
 ON e.id = emp.managerId
 GROUP BY e.id, e.name
-HAVING COUNT(emp.id) >= 5
+HAVING COUNT(emp.id) >= 5 #we are filtering groups instad of rows so we use having here
