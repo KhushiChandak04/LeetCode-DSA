2b0ca1c61af238a88e70ff41ece4623e16849af4
Khushi Chandak <khushichandak2005@gmail.com>
2026-07-14 21:29:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (65.19%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        #TO MAINTAIN O(logn) complexity, we will use 2 ptr approach
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right: #binary search
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
