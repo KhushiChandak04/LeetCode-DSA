1f3532bba33695ed04e6dbe05ffcadbd8213e68b
Khushi Chandak <khushichandak2005@gmail.com>
2026-08-26 21:37:45 +0530

    Time: 1139 ms (35.55%) | Memory: 21.6 MB (29.17%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        def mergeSort(arr):
+            if len(arr) <= 1:
+                return arr
+            
+            mid = len(arr) // 2
+            left = mergeSort(arr[:mid])
+            right = mergeSort(arr[mid:])
+
+            result = []
+            i, j = 0,0
+
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    result.append(left[i])
+                    i += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+
+            while i < len(left): #if the elements are remaining
+                result.append(left[i])
+                i += 1
+            while j < len(right):
+                result.append(right[j])
+                j += 1
+            
+            return result
+        return mergeSort(nums)
